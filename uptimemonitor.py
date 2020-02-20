3d8d9a0f8a8bf3fdb8f1cca7c6d678bc27aefbb4
blaqguy <54968720+blaqguy@users.noreply.github.com>
2020-02-20 08:12:16 -0500

    Update uptimemonitor.py

--- uptimemonitor.py
+++ uptimemonitor.py
@@ -1,120 +1,94 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
-from slackclient import SlackClient
+#---Dependencies; must pip install these
 import requests
-import datetime
+
+
+#---These are part of the standard python library, no need to pip install
 import pickle
-
-# ================== CONFIGURATION ==================
-
-URLS_TO_CHECK = ["http://example.com",
-                 "https://anotherone.net:1337"]
+import datetime
+import json
+import time
 
 
-# You have to have a token so you can send messages to a slack channel!
-# https://api.slack.com/docs/oauth-test-tokens
-SLACK_TOKEN = "YOUR_TOKEN_HERE"
-
-# Where you want to receive your notifications ?
-
-# you can get it using list_channels and channel_info or click on the channel name
-# click on add app or integration and you can view it's id in the webpage redirection (you have to be fast because it redirects it to another page)
-CHANNEL_ID = "G2LCBCXGF"
-
-# ================== END CONFIGURATION ==================
-
-slack_client = SlackClient(SLACK_TOKEN)
+#---Config
+url_list = ['https://jss.intersections.com:8443/healthCheck.html']
+#The incoming webhook url from slack, google how to create this if you don't already have one
+slack_webhook = ''
 
 
-# ================== SLACK FUNCTIONS ==================
-def list_channels():
-    channels_call = slack_client.api_call("channels.list")
-    if channels_call['ok']:
-        return channels_call['channels']
-    return None
+#---Slack Function
+def notify_slack(message):
+    body = json.dumps({'text': message})
+    #body = json.dumps({'username': 'Website_Monitor', 'icon_emoji': ':chrislook:', 'text': message}) #Commented this out as I'm using the webhook and the username and Icon along with other properties are already predefined
+    payload = requests.post(slack_webhook,data=body, verify=False) #Verify=false We Man in the Middle egress SSL/HTTPS traffic, remove if not using from Corp Network
 
 
-def channel_info(channel_id):
-    channel_info = slack_client.api_call("channels.info", channel=channel_id)
-    if channel_info:
-        return channel_info['channel']
-    return None
-
-
-def send_message(channel_id, message):
-    response = slack_client.api_call(
-        "chat.postMessage",
-        channel=channel_id,
-        text=message,
-        username='UptimeMonitor',
-        icon_emoji=':robot_face:'
-    )
-    if not response['ok']:
-        print("Error sending notification to Slack -------> " + response['error'])
-
-
-# ================== NOTIFY FUNCTIONS ==================
-def down_notifier(the_url):
+#---URL Status Functions
+def down(url):
+    #Try unpickling the pickled file
     try:
-        old_status_file = pickle.load(open("status.p", "rb"))
+        current_status_file = pickle.load(open("url_status.p", "rb"))
+    #If not found, print the statement, assume url is down and send message to slack then exit the function
     except Exception as ex:
-        print("The status.p file was not found. it will be recreated." + str(ex))
-        send_message(CHANNEL_ID, ":no_entry: " + the_url + " is down.")
+        print("The url_status.p file was not found. it will be recreated." + str(ex))
+        notify_slack(url + " *is down!* :no_entry:")
         return
-
-    # in case we just added the url to our database
-    if the_url not in old_status_file:
-        send_message(CHANNEL_ID, ":no_entry: " + the_url + " is down.")
+    #In the if status code not equal to 200 block below, the down function is evaluated before an entry is made in the dictionary. This statement accounts for that. Assume url is down and notify slack then exit the function.
+    if url not in current_status_file:
+        notify_slack(url + " *is down!* :no_entry:")
         return
-
-    # if it's already in the database and last status was up
-    if (the_url in old_status_file) and (old_status_file[the_url]['status'] == "up"):
-        send_message(CHANNEL_ID, ":no_entry: " + the_url + " is down.")
+    #If the status code not equal 200 and the last status was up, assume down and notify slack
+    if (url in current_status_file) and (current_status_file[url]['status'] == "up"):
+        notify_slack(url + " *is down!* :no_entry:")
+    #If not any of the previous condition, assume the url was already down, don't notify
     else:
-        print("already down. skip notifying")
+        print("already down, skipping notify")
 
-
-# if it was previously down, notify that it's back online
-def back_online_notifier(the_url):
-    print("found " + the_url + " back online.")
+def back_online(url):
+    print("found " + url + " back online.")
     try:
-        old_status_file = pickle.load(open("status.p", "rb"))
+        current_status_file = pickle.load(open("url_status.p", "rb"))
     except Exception as ex:
-        print("The status.p file was not found. it will be recreated." + str(ex))
+        print("The url_status.p file was not found. it will be recreated." + str(ex))
         return
 
-    if (the_url in old_status_file) and (old_status_file[the_url]['status'] == "down"):
-        it_was_down_time = old_status_file[the_url]['time']
-        current_time = datetime.datetime.now().replace(microsecond=0)
-        send_message(CHANNEL_ID, ":herb: " + the_url + " is back online. It was down for " + str(current_time - it_was_down_time))
+    #If status code equal 200 and the last status was down, assume url is reachable and notify slack
+    if (url in current_status_file) and (current_status_file[url]['status'] == "down"):
+        it_was_down_time = current_status_file[url]['time']
+        current_time = datetime.datetime.now()
+        notify_slack(url + " *is back online!* :up:, it was down for" + str(current_time - it_was_down_time))
+    #If none of the previous statements are evaluated, assume url is still/has been reachable, no notification
     else:
         print("skipping notifying that the url is online")
 
 
-# --- getting only the head ---
-def get_status_code(xurl):
-    try:
-        response = requests.head(xurl, allow_redirects=False, timeout=3)
-        return response.status_code
-    except:
-        return -1
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~ THE MAIN ~~~~~~~~~~~~~~~~~~~~~~~~
+#---Execution
+#Python checks to see the code you're running it's being run directly by python or if it's imported. If being run by python-
+#for example in the terminal you type, python3 <some python file>, python will set __name__ to be equal to main.
+#The "if __name__ == '__main__':" is used as sort of an entry point in the program/script. Telling python to run this block first(?)
 if __name__ == '__main__':
+    #The While true is here to keep this code block running indefinitely
+    while True:
+        #An empty dictionary to store the status of our urls
+        url_status = {}
+        for url in url_list:
+            try:
+                #Use requests lib to get status code of the url
+                r = requests.get(url, timeout=10)
+                #If status code isn't a 200, run this block
+                if r.status_code != 200:
+                    print(url + " is down.")
+                    down(url)
+                    url_status[url] = {'status': "down", 'time': datetime.datetime.now()}
+                #If status code is 200, run this block
+                else:
+                    back_online(url)
+                    url_status[url] = {'status': "up", 'time': datetime.datetime.now()}
+                    time.sleep(10)
+            #This block is to handle the connection timing out exception. Assume url is unreachable if connection times out       
+            except requests.ConnectionError:
+                print(url + " is down.")
+                down(url)
+                url_status[url] = {'status': "down", 'time': datetime.datetime.now()}
 
-    # First test if you have the correct token
-    # print(slack_client.api_call("auth.test"))
-
-    status_file = {}
-
-    for url in URLS_TO_CHECK:
-        if get_status_code(url) != 200:
-            print(url + " is down.")
-            down_notifier(url)
-            status_file[url] = {'status': "down", 'time': datetime.datetime.now().replace(microsecond=0)}
-        else:
-            back_online_notifier(url)
-            status_file[url] = {'status': "up", 'time': datetime.datetime.now().replace(microsecond=0)}
-
-        pickle.dump(status_file, open("status.p", "wb"))
+        #Dump the values stored in memory for the url_status dictionary to the url_status pickle file
+        pickle.dump(url_status, open("url_status.p", "wb"))
